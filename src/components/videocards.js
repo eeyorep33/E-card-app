15b355fd165f992d0502d0e2e804c884ce374c21
DESKTOP-JKLU25P\Jaime <eeyorep33@gmail.com>
2018-03-27 14:37:26 -0500

    console log this

--- src/components/videocards.js
+++ src/components/videocards.js
@@ -4,7 +4,7 @@ import Details from './details'
 class VideoCards extends Component {
      
       componentDidMount(){
-            console.log(this.props)
+            console.log(this)
       }
       
      
