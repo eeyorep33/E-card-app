f4fabb6b7e90310b17bbdb1839e15f6a6ead848e
DESKTOP-JKLU25P\Jaime <eeyorep33@gmail.com>
2018-03-27 15:16:57 -0500

    passed play prop

--- src/components/videocards.js
+++ src/components/videocards.js
@@ -39,7 +39,7 @@ class VideoCards extends Component {
                   <div>
                         {cards_page(location.pathname)}
                         <Switch>
-                              <Route path={match.url + '/:id'} render={(props) => <Details submit={this.props.submit} current={this.props.current}cards={this.props.videos}{...props} />} />
+                              <Route path={match.url + '/:id'} render={(props) => <Details play={this.props.play}submit={this.props.submit} current={this.props.current}cards={this.props.videos}{...props} />} />
                         </Switch>
                   </div>
             )
