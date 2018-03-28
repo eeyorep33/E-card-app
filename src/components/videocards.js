1a8f83dcfdc058d8aaf7eb7656266e51c6cb91fa
DESKTOP-JKLU25P\Jaime <eeyorep33@gmail.com>
2018-03-28 10:46:25 -0500

    more pause icon

--- src/components/videocards.js
+++ src/components/videocards.js
@@ -6,6 +6,7 @@ class VideoCards extends Component {
       render() {
             const { match, location } = this.props
             const cards_page = (path) => {
+                  console.log(this.props.status)
                   if (path === '/videocards/easter'
                         || path === '/videocards/birthday'
                         || path === '/videocards/anniversary'
@@ -20,7 +21,7 @@ class VideoCards extends Component {
                                           <div className="col-4 vidContainer">
                                                 <video className="vid" controls ref={`video${i}`} src={item.source}></video>
                                                 <button className="playButton" onClick={() => this.props.play(item.id, this.refs["video" + i])}>
-                                                <i className={this.props.status ? "fas fa-pause":"fas fa-play"}></i></button>
+                                                <i className={this.props.status=true ? "fas fa-pause":"fas fa-play"}></i></button>
                                                 <Link to={match.url + '/' + item.id}><p className="vidName">{item.name}</p>
                                                 </Link>
                                           </div>
