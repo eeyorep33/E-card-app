c1af83e3cad8d83603ef1af440dc84e2b12544f9
DESKTOP-JKLU25P\Jaime <eeyorep33@gmail.com>
2018-03-28 10:32:18 -0500

    working on pause

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       username: "",
       email: "",
       videocards: [],
-      status:"paused"
+      status:false
     }
   }
   componentWillMount() {
@@ -43,11 +43,11 @@ class App extends Component {
       })
   }
   play = (id, ref) => {
-if(this.state.status==="paused"){
-  this.setState({status:play}, ()=>{ref.play()}) 
+if(this.state.status===false){
+  this.setState({status:true}, ()=>{ref.play()}) 
 }
-else if(this.state.status==="play"){
-  this.setState({status:"paused"}, ()=>{ref.pause()})
+else if(this.state.status===true){
+  this.setState({status:false}, ()=>{ref.pause()})
 }
    
        
