55556cf06d791a80e57f118cae9d68c3964c6aa5
DESKTOP-JKLU25P\Jaime <eeyorep33@gmail.com>
2018-03-28 13:57:23 -0500

    fixing button css

--- src/components/details.js
+++ src/components/details.js
@@ -4,9 +4,9 @@ class Details extends Component {
             let ending = this.props.cards[i].source.substr(this.props.cards[i].source.length - 3, 3)
             if (ending === "mp4") {
                   return <div className="vidContainer"><video className="displayVid" ref={`specVideo`} src={this.props.cards[i].source}></video>
-                        <button className={this.props.status==="play"?"playButton":"playButton hidden" } onClick={() => this.props.play(i, this.refs.specVideo)}>
+                        <button className={this.props.status==="play"?"displayPlayplayButton":"displayPlay hidden" } onClick={() => this.props.play(i, this.refs.specVideo)}>
                         <i className="fas fa-pause"></i></button>
-                        <button className={this.props.status==="paused"?"playButton":"playButton hidden" } onClick={() => this.props.play(i, this.refs.specVideo)}>
+                        <button className={this.props.status==="paused"?"displayPlay":"displayPlay hidden" } onClick={() => this.props.play(i, this.refs.specVideo)}>
                        <i className="fas fa-play"></i></button>
                   </div>
             }
